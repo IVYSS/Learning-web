e42426b3885aa3b01352e68709691ccf4206fce5
ivy <61070075@kmitl.ac.th>
2026-05-07 14:33:44 +0700

    page/about-us/  create page and add context

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import { AboutGeeksForGeeks } from "./components/AboutGeeksForGeeks";
+
+export default function AboutPage() {
+  return (
+    <main className=" px-6 py-10 ">
+      <h1 className="text-[32px] font-semibold mb-6">About us</h1>
+      <AboutGeeksForGeeks />
+    </main>
+  );
+}
